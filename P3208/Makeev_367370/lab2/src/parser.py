fa21dcf4f080feb3a95a5a06a4fe08f8da9adbe4
gl4zis <maker64@inbox.ru>
2024-03-24 00:59:19 +0300

    parser unification

--- P3208/Makeev_367370/lab2/src/parser.py
+++ P3208/Makeev_367370/lab2/src/parser.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from typing import Callable
+
 import method
 import equation
 from equiation_type import EquationType
@@ -6,42 +9,31 @@ from equiation_type import EquationType
 class Parser:
     @staticmethod
     def choose_method(for_sys: bool) -> method.Method:
-        counter = 1
-        for m in method.MethodType:
-            if m.value.for_sys == for_sys:
-                print('\t', counter, ': ', m.value, sep='')
-                counter += 1
-
-        num: str = ''
-        rang: range = range(1, len(method.MethodType) + 1)
-        while True:
-            try:
-                if int(num) in rang:
-                    break
-            except ValueError:
-                pass
-            num = input('Choose method: [' + ('/'.join(str(x) for x in rang)) + '] -> ')
-
-        return list(method.MethodType)[int(num) - 1].value
+        return Parser.__choose_enum(method.MethodType, 'method', lambda m: m.for_sys == for_sys)
 
     @staticmethod
     def choose_single_eq() -> equation.Equation:
+        return Parser.__choose_enum(EquationType, 'equation')
+
+    @staticmethod
+    def __choose_enum(enum: Enum, name: str, cond: Callable = lambda o: True):
         counter = 1
-        for eq in EquationType:
-            print('\t', counter, ': ', eq.value, sep='')
-            counter += 1
+        for obj in enum:
+            if cond(obj.value):
+                print('\t', counter, ': ', obj.value, sep='')
+                counter += 1
 
         num: str = ''
-        rang: range = range(1, len(EquationType) + 1)
+        rang: range = range(1, len(enum) + 1)
         while True:
             try:
                 if int(num) in rang:
                     break
             except ValueError:
                 pass
-            num = input('Choose equation: [' + ('/'.join(str(x) for x in rang)) + '] -> ')
+            num = input('Choose ' + name + ': [' + ('/'.join(str(x) for x in rang)) + '] -> ')
 
-        return list(EquationType)[int(num) - 1].value
+        return list(enum)[int(num) - 1].value
 
     @staticmethod
     def choose_system_eq() -> equation.EquationSystem:
